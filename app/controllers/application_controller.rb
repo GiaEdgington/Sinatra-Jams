37ababc5aaebcc927e5ef707a12fde1f9c497f30
Gia Edgington <giaedgington@Erics-MacBook.local>
2019-06-26 11:09:21 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 require './config/environment'
+require 'pry'
 
 class ApplicationController < Sinatra::Base
 
@@ -9,6 +10,46 @@ class ApplicationController < Sinatra::Base
 
   get "/" do
     erb :welcome
+  end
+
+  get '/artists' do
+    @artists = Artist.all
+    erb :index
+  end
+
+  get '/artists/new' do
+    erb :new
+  end
+
+  #Create Action
+  post '/artists' do
+    @artist = Artist.create(params)
+    redirect to "/artists/#{@artist.id}"
+  end
+
+  #Show Action
+  get "/artists/:id" do
+    @artist = Artist.find(params[:id])
+    erb :show
+  end
+
+  #Edit Action
+  get "/artists/:id/edit" do
+    @artist = Artist.find(params[:id])
+    erb :edit
+  end
+
+  put "/artists/:id" do
+    @artist = Artist.find(params[:id])
+    @artist.update(params[:artist])
+
+    redirect to "/artists/#{@artist.id}"
+  end
+
+  delete "/artists/:id" do
+    @artist = Artist.find(params[:id])
+    @artist.destroy
+    redirect to "/artists"
   end
 
 end
